07caca35a90538cec4fbc28957837ad5468f5b55
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2017-09-22 09:50:03 +0900

    add result json

--- modules/adapter/lib/serverless/api/src/main/scala/com/github/yoshiyoshifujii/s8scala/infrastructure/lambda/ResponseJson.scala
+++ modules/adapter/lib/serverless/api/src/main/scala/com/github/yoshiyoshifujii/s8scala/infrastructure/lambda/ResponseJson.scala
@@ -1,7 +1,13 @@
 package com.github.yoshiyoshifujii.s8scala.infrastructure.lambda
 
 import com.github.yoshiyoshifujii.s8scala.infrastructure.lambda.HttpStatusCode.HttpStatusCode
+import spray.json.DefaultJsonProtocol
 
 case class ResponseJson(isBase64Encoded: Boolean,
                         statusCode: HttpStatusCode,
-                        headers: Map[String, String])
+                        headers: Map[String, String],
+                        body: Option[String])
+
+object ResponseJsonProtocol extends DefaultJsonProtocol {
+  implicit val responseJsonFormatter = jsonFormat4(ResponseJson)
+}
---
+++ modules/adapter/lib/serverless/api/src/test/scala/com/github/yoshiyoshifujii/s8scala/infrastructure/lambda/ResponseJsonProtocolSpec.scala
@@ -0,0 +1,28 @@
+package com.github.yoshiyoshifujii.s8scala.infrastructure.lambda
+
+import org.specs2.mutable.Specification
+import spray.json._
+
+class ResponseJsonProtocolSpec extends Specification {
+
+  "ResponseJsonProtocol" should {
+    "success" in {
+      import ResponseJsonProtocol._
+      ResponseJson(
+        isBase64Encoded = false,
+        statusCode = HttpStatusCode.OK,
+        headers = Map(
+          "Content-Type" -> "application/json"
+        ),
+        body = Some("{}")
+      ).toJson must_== JsObject(
+        "isBase64Encoded" -> JsFalse,
+        "statusCode" -> JsNumber(200),
+        "headers" -> JsObject(
+          "Content-Type" -> JsString("application/json")
+        ),
+        "body" -> JsString("{}")
+      )
+    }
+  }
+}
